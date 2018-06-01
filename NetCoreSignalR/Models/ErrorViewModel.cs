7f9c4994b1746b6e553ecebfdb125f735857cf63
tim <1510899867@qq.com>
2018-06-01 14:23:38 +0800

    添加项目文件。

---
+++ NetCoreSignalR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreSignalR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
